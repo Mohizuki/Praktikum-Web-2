1b6a445bdfc96d46fa5c832b9c9be46cefbd52cd
Crunchii <32230454+Mohizuki@users.noreply.github.com>
2022-03-24 18:39:50 +0800

    Update PRAK103.php

--- modul1/PRAK103.php
+++ modul1/PRAK103.php
@@ -4,9 +4,7 @@ $fahren = ($celcius*9/5) + 32;
 $reamur = $celcius*4/5;
 $kelvin = $celcius + 273.15;
 
-
 echo "Fahrenheit (F) =".round($fahren,4)."<br>";
 echo "Reamur (R) = ".round($reamur,4)."<br>";
 echo "Kelvin (K) = ".round($kelvin,4)."<br>";
-
 ?>
